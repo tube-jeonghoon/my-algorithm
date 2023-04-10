6bbda6767451c36c867b4c33c032d64a1ee85e93
tube-jeonghoon <55288856+tube-jeonghoon@users.noreply.github.com>
2023-04-10 15:17:28 +0900

    Update 나누어 떨어지는 숫자 배열.js

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -1,8 +1,10 @@
 let solution = (arr, divisor) => {
   const result = arr.filter((num) => num % divisor === 0);
-  if (result.length === 0) {
-    return [-1];
-  } else {
-    return result.sort((a, b) => a - b);
-  }
+//   if (result.length === 0) {
+//     return [-1];
+//   } else {
+//     return result.sort((a, b) => a - b);
+//   }
+  // 삼항연사자 사용
+  return result.length === 0 ? [-1] : result.sort((a, b) => a - b);
 };
